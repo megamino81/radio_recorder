d310f88edd4768a4245f391be32d5c6be2b5e2df
Minho Jung <megamino@gmail.com>
2024-08-29 17:10:36 +0900

    fix mbc_start_night.sh to try 5 times to get URL

--- mbc_star_night.sh
+++ mbc_star_night.sh
@@ -4,10 +4,15 @@ NAME=MBC_star_night
 SECS=7200
 OUT_DIR=data
 
-#URL="http://serpent0.duckdns.org:8088/mbcfm.pls"
-output=$(curl -s 'http://serpent0.duckdns.org:8088/mbcfm.pls')
-URL=$(echo "$output" | grep '^File1=' | sed 's/File1=//')
+URL=""
+attempt=0
+max_attempt=5
 
+while [ -z "$URL" ] && [ "$attempt" -lt "$max_attempt" ]; do
+    output=$(curl -s 'http://serpent0.duckdns.org:8088/mbcfm.pls')
+    URL=$(echo "$output" | grep '^File1=' | sed 's/File1=//')
+    attempt=$((attempt+1))
+done
 
 ABS_PATH=$(readlink -f "$0")
 ABS_DIR=$(dirname "$ABS_PATH")
